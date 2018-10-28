800249aaefa71decc91017331db3c940fa43910e
tommydrama <tomek1darowski@gmail.com>
2018-10-28 10:56:23 +0100

    servlet test

--- src/main/java/com/sda/HelloServlet.java
+++ src/main/java/com/sda/HelloServlet.java
@@ -1,5 +1,7 @@
 package com.sda;
 
+import org.apache.commons.lang3.StringUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,7 +13,17 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String name = req.getParameter("name");
+        String nameToDisplay = StringUtils.isEmpty(name) ? "Annonymous" : name;
+
+//        if(StringUtils.isEmpty(name)){
+//            nameToDisplay = "Annonymous";
+//        }else{
+//            nameToDisplay = name;
+//        }
+
         PrintWriter writer = resp.getWriter();
-        writer.print("<h1>Hello World</h1>");
+        writer.print("<h1>Hello "  + name+ "</h1>");
     }
 }
